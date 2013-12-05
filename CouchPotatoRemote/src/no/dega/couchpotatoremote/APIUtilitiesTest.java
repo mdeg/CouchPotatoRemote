66abd256f0c92f229b1f0c9ab4d8765688afa2f3
mdeg <michael@dega.no>
2013-12-05 20:38:34 +0800

    Changed project name/package, bugfixes on API parsing

---
+++ CouchPotatoRemote/src/no/dega/couchpotatoremote/APIUtilitiesTest.java
@@ -0,0 +1,28 @@
+package no.dega.couchpotatoremote;
+
+
+import junit.framework.TestCase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class APIUtilitiesTest extends TestCase {
+
+	public void testMakeRequest() {
+		//TODO: this will fail - need to get proper context for this
+		String response = APIUtilities.makeRequest("app.available", null);
+		assertNotNull("Response string is null", response);
+		assertFalse("Response is 0-length", response.length() <= 0);
+		
+		try {
+			JSONObject jsonResponse = new JSONObject(response);
+			assertNotNull("JSON created from response is null", jsonResponse);
+			assertFalse("JSON created from response is 0-length", jsonResponse.length() <= 0);
+	
+			assertTrue("API not operational.", jsonResponse.getBoolean("success"));
+		} catch (JSONException e) {
+			fail("JSONException caused by: " + e.getCause());
+		}
+	}
+
+}
---
+++ CouchPotatoRemote/src/no/dega/couchpotatoremote/MovieViewActivity.java
@@ -0,0 +1,54 @@
+package no.dega.couchpotatoremote;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+public class MovieViewActivity extends Activity {
+	
+	public MovieViewActivity() {
+		
+	}
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_movie_view);
+
+	    getActionBar().setDisplayHomeAsUpEnabled(true);
+
+		
+		Bundle bun = getIntent().getExtras();
+		TextView movieTitle = (TextView) findViewById(R.id.movieview_title);
+		TextView moviePlot = (TextView) findViewById(R.id.movieview_plot);
+		TextView movieTagline = (TextView) findViewById(R.id.movieview_tagline);
+		TextView movieYear = (TextView) findViewById(R.id.movieview_year);
+		ImageView poster = (ImageView) findViewById(R.id.movieview_poster);
+		
+		if(bun != null) {
+			Movie movie = bun.getParcelable("no.dega.couchpotatoremote.Movie");
+
+            if(movie == null) {
+                Log.e("MovieViewActivity", "Movie passed to MovieView is null (parcelling may have failed)");
+            } else {
+
+                Log.d("AfterParcel", movie.toString());
+
+                this.setTitle(movie.getTitle());
+
+                movieTitle.setText(movie.getTitle());
+                moviePlot.setText(movie.getPlot());
+                movieTagline.setText(movie.getTagline());
+                movieYear.setText(String.valueOf(movie.getYear()));
+                //Grab from cache, or network if not cached
+                ImageLoader.getInstance().displayImage(movie.getPosterUri(), poster);
+            }
+		} else {
+			Log.e("MovieViewActivity", "Null bundle passed to movieview");
+		}
+	}
+}
