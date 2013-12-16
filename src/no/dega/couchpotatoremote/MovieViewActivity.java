34a8bea3821f3314872b284b6c8ad1fb2c8410ea
mdeg <michael@dega.no>
2013-12-16 17:29:29 +0800

    Collapse dropdowns on movie switch

--- src/no/dega/couchpotatoremote/MovieViewActivity.java
+++ src/no/dega/couchpotatoremote/MovieViewActivity.java
@@ -5,7 +5,6 @@ import android.support.v4.app.DialogFragment;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -13,7 +12,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -25,14 +23,13 @@ import java.util.ArrayList;
     When a user clicks on a movie in the list, take them to this view activity
     User can see poster, title, tagline, year, plot and actors/directors
 */
-    //TODO: add sideways scrolling
 public class MovieViewActivity extends ActionBarActivity {
-    ArrayList<Movie> movies = null;
-    Movie current = null;
-    int currentPos = 0;
-    boolean actorsExpanded = false;
-    boolean directorsExpanded = false;
-    GestureDetector gestureDetector = null;
+    private ArrayList<Movie> movies = null;
+    private Movie current = null;
+    private int currentPos = 0;
+    protected boolean actorsExpanded = false;
+    protected boolean directorsExpanded = false;
+    private GestureDetector gestureDetector = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -77,8 +74,11 @@ public class MovieViewActivity extends ActionBarActivity {
                     @Override
                     public void onAnimationRepeat(Animation animation) {}
                 });
+                //Collapse the dropdowns
+                hideActors();
+                hideDirectors();
+
                 layout.startAnimation(slideoutMovie);
-                //TODO: reset dropdown buttons here
                 //TODO: take velocity and use it to set the time of the animation
                 return true;
             }
@@ -135,60 +135,75 @@ public class MovieViewActivity extends ActionBarActivity {
     //Called when user presses 'Actors' button. Expands list of actors.
     public void onActorButtonPress(View view) {
         //TODO: add some animation for expanding/unexpanding
-        TextView actors = (TextView) findViewById(R.id.movieview_actors_text);
-
         if(!actorsExpanded) {
-            actorsExpanded = true;
-
-            StringBuilder display = new StringBuilder();
-            if(current.getActors().length > 0) {
-                for(String str: current.getActors()) {
-                    display.append(str).append("\n");
-                }
-            } else {
-                display.append("No actors.");
-            }
-
-            actors.setText(display.toString());
-            actors.setVisibility(View.VISIBLE);
+            showActors();
         } else {
-            //Already expanded, and we need to close
-            actorsExpanded = false;
-            actors.setVisibility(View.GONE);
+            hideActors();
         }
+    }
+
+    private void showActors() {
+        TextView actors = (TextView) findViewById(R.id.movieview_actors_text);
+        actorsExpanded = true;
+
+        StringBuilder display = new StringBuilder();
+        if(current.getActors().length > 0) {
+            for(String str: current.getActors()) {
+                display.append(str).append("\n");
+            }
+        } else {
+            display.append("No actors.");
+        }
+
+        actors.setText(display.toString());
+        actors.setVisibility(View.VISIBLE);
+    }
+
+    //Hide actors list
+    private void hideActors() {
+        TextView actors = (TextView) findViewById(R.id.movieview_actors_text);
+        actorsExpanded = false;
+        actors.setVisibility(View.GONE);
     }
 
     //Called when user presses 'Directors' button. Expands list of directors.
     public void onDirectorButtonPress(View view) {
         //TODO: add some animation for expanding/unexpanding
-        TextView directors = (TextView) findViewById(R.id.movieview_directors_text);
-
         if(!directorsExpanded) {
-            directorsExpanded = true;
-
-            StringBuilder display = new StringBuilder();
-            if(current.getDirectors().length > 0) {
-                for(String str: current.getDirectors()) {
-                    display.append(str).append("\n");
-                }
-            } else {
-                display.append("No directors.");
-            }
-
-            directors.setText(display.toString());
-            directors.setVisibility(View.VISIBLE);
+            showDirectors();
         } else {
-            //Already expanded, and we need to close
-            directorsExpanded = false;
-            directors.setVisibility(View.GONE);
+            hideDirectors();
         }
     }
+    private void showDirectors() {
+        TextView directors = (TextView) findViewById(R.id.movieview_directors_text);
+        directorsExpanded = true;
+
+        StringBuilder display = new StringBuilder();
+        if(current.getDirectors().length > 0) {
+            for(String str: current.getDirectors()) {
+                display.append(str).append("\n");
+            }
+        } else {
+            display.append("No directors.");
+        }
+
+        directors.setText(display.toString());
+        directors.setVisibility(View.VISIBLE);
+    }
+    private void hideDirectors() {
+        TextView directors = (TextView) findViewById(R.id.movieview_directors_text);
+        //Already expanded, and we need to close
+        directorsExpanded = false;
+        directors.setVisibility(View.GONE);
+    }
+    //Alert dialog to confirm that the user really wants to delete a movie
     private class ConfirmMovieDeleteFragment extends DialogFragment {
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             builder.setMessage(R.string.confirm_delete_movie)
-
+                //Confirm delete
                 .setPositiveButton(R.string.accept_delete_movie, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
@@ -200,15 +215,11 @@ public class MovieViewActivity extends ActionBarActivity {
                         finish();
                     }
                 })
-
+                //Reject delete
                 .setNegativeButton(R.string.reject_delete_movie, new DialogInterface.OnClickListener() {
                 @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    //Do something?
-                }
-
-            }).show();
-
+                public void onClick(DialogInterface dialogInterface, int i) { }
+            });
         return builder.create();
         }
     }
