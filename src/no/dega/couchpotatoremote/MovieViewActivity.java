754ba2acba7331dbec8faf4440a1019e8259b4bf
mdeg <michael@dega.no>
2013-12-18 17:09:26 +0800

    Added edit qualities

--- src/no/dega/couchpotatoremote/MovieViewActivity.java
+++ src/no/dega/couchpotatoremote/MovieViewActivity.java
@@ -1,23 +1,34 @@
 package no.dega.couchpotatoremote;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.support.v4.app.DialogFragment;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /*
     When a user clicks on a movie in the list, take them to this view activity
@@ -40,6 +51,9 @@ public class MovieViewActivity extends ActionBarActivity {
 
         setContentView(R.layout.activity_movie_view);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
         //Create the fling listener
         GestureDetector.SimpleOnGestureListener listener =
                 new GestureDetector.SimpleOnGestureListener() {
@@ -80,6 +94,7 @@ public class MovieViewActivity extends ActionBarActivity {
                 //Collapse the dropdowns
                 hideActors();
                 hideDirectors();
+                hidePlot();
 
                 layout.startAnimation(slideoutMovie);
                 //TODO: take velocity and use it to set the time of the animation
@@ -111,14 +126,12 @@ public class MovieViewActivity extends ActionBarActivity {
         this.setTitle(current.getTitle());
 
         TextView movieTitle = (TextView) findViewById(R.id.movieview_title);
-      //  TextView moviePlot = (TextView) findViewById(R.id.movieview_plot);
         TextView movieTagline = (TextView) findViewById(R.id.movieview_tagline);
         TextView movieYear = (TextView) findViewById(R.id.movieview_year);
         ImageView poster = (ImageView) findViewById(R.id.movieview_poster);
 
         movieTitle.setText(current.getTitle());
-      //  moviePlot.setText(current.getPlot());
-        //We want to hide the tagline if there isn't one, helps compact the view
+        //Hide the tagline if there isn't one, helps compact the view
         if(current.getTagline().length() > 0) {
             movieTagline.setText(current.getTagline());
         } else {
@@ -144,7 +157,7 @@ public class MovieViewActivity extends ActionBarActivity {
     }
 
     private void showPlot() {
-        TextView plot = (TextView) findViewById(R.id.movieview_plot);
+        TextView plot = (TextView) findViewById(R.id.movieview_plot_text);
         plotExpanded = true;
 
         plot.setText(current.getPlot());
@@ -152,7 +165,7 @@ public class MovieViewActivity extends ActionBarActivity {
     }
 
     private void hidePlot() {
-        TextView plot = (TextView) findViewById(R.id.movieview_plot);
+        TextView plot = (TextView) findViewById(R.id.movieview_plot_text);
         plotExpanded = false;
         plot.setVisibility(View.GONE);
     }
@@ -200,6 +213,7 @@ public class MovieViewActivity extends ActionBarActivity {
             hideDirectors();
         }
     }
+
     private void showDirectors() {
         TextView directors = (TextView) findViewById(R.id.movieview_directors_text);
         directorsExpanded = true;
@@ -216,12 +230,121 @@ public class MovieViewActivity extends ActionBarActivity {
         directors.setText(display.toString());
         directors.setVisibility(View.VISIBLE);
     }
+
     private void hideDirectors() {
         TextView directors = (TextView) findViewById(R.id.movieview_directors_text);
         //Already expanded, and we need to close
         directorsExpanded = false;
         directors.setVisibility(View.GONE);
     }
+
+    public void onQualityButtonPress(View view) {
+        new ChangeQualityFragment().show(getSupportFragmentManager(), null);
+    }
+
+
+
+
+    //Popup box for user to change qualities
+    private class ChangeQualityFragment extends DialogFragment {
+        ArrayList<Quality> qualities = null;
+        Spinner spinner = null;
+        private Quality selection = null;
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            setRetainInstance(true);
+
+            View view = getActivity().getLayoutInflater().inflate(R.layout.change_quality_dialog, null);
+            spinner = (Spinner) view.findViewById(R.id.changequality_selector);
+            //Get a list of qualities
+            new GetQualitiesTask(MovieViewActivity.this).execute(APIUtilities.formatRequest("quality.list", MovieViewActivity.this));
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+            builder.setView(view)
+                    //Accept the quality change
+                    .setPositiveButton(R.string.accept_change_quality, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            StringBuilder builder = new StringBuilder();
+                            builder.append("movie.edit?").append("id=").append(current.getLibraryId())
+                                    .append("&profile_id=").append(selection.getId());
+                            new APIRequestAsyncTask<String, Void, String>(MovieViewActivity.this)
+                                    .execute(APIUtilities.formatRequest(builder.toString(), MovieViewActivity.this));
+
+                            Toast.makeText(MovieViewActivity.this, current.getTitle() + "'s quality changed to " + selection.getLabel() + ".",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .setNegativeButton(R.string.reject_change_quality, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {}
+                    });
+            return builder.create();
+        }
+
+        private class GetQualitiesTask extends APIRequestAsyncTask<String, Void, String> {
+            public GetQualitiesTask(Context context) {
+                super(context);
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                parseQualitiesList(result);
+
+                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
+                        selection = qualities.get(pos);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+                    }
+                });
+
+                ArrayAdapter<Quality> adapter = new ArrayAdapter<Quality>(MovieViewActivity.this,
+                        R.layout.spinner_dropdown_tight, qualities);
+                spinner.setAdapter(adapter);
+            }
+
+            private void parseQualitiesList(String result) {
+                try {
+                    JSONArray list = new JSONObject(result).getJSONArray("list");
+                    qualities = new ArrayList<Quality>(list.length());
+                    for(int i = 0; i < list.length(); i++) {
+                        JSONObject quality = list.getJSONObject(i);
+                        Quality qual = new Quality(quality.getInt("id"), quality.getString("label"));
+                        qualities.add(qual);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }
+        private class Quality {
+            private final int id;
+            private final String label;
+
+            public Quality(int id, String label) {
+                this.id = id;
+                this.label = label;
+            }
+            public String getLabel() {
+                return label;
+            }
+            public int getId() {
+                return id;
+            }
+            public String toString() {
+                return label;
+            }
+        }
+    }
+
     //Alert dialog to confirm that the user really wants to delete a movie
     private class ConfirmMovieDeleteFragment extends DialogFragment {
         @Override
@@ -233,7 +356,7 @@ public class MovieViewActivity extends ActionBarActivity {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         String request = APIUtilities.formatRequest(
-                            "movie.delete?id=" + String.valueOf(current.getLibraryId()), getActivity());
+                                "movie.delete?id=" + String.valueOf(current.getLibraryId()), getActivity());
                         //Send the request. Don't need to subclass this
                         new APIRequestAsyncTask<String, Void, String>(getActivity()).execute(request);
                         movies.remove(currentPos);
@@ -242,9 +365,10 @@ public class MovieViewActivity extends ActionBarActivity {
                 })
                 //Reject delete
                 .setNegativeButton(R.string.reject_delete_movie, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) { }
-            });
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
         return builder.create();
         }
     }
