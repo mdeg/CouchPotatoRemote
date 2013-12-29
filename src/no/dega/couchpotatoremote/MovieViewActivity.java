d8e92e1dbb0859b9f0a8ab3d39ab55ef12be3733
mdeg <michael@dega.no>
2013-12-29 21:24:41 +0800

    Fixed fling bug; fixed extra blank text line bug

--- src/no/dega/couchpotatoremote/MovieViewActivity.java
+++ src/no/dega/couchpotatoremote/MovieViewActivity.java
@@ -20,9 +20,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -45,6 +43,18 @@ public class MovieViewActivity extends ActionBarActivity {
     protected boolean plotExpanded = false;
     private GestureDetector gestureDetector = null;
 
+    //Parameters for a swipe to qualify
+    private static final int FLING_MIN_DISTANCE = 120;
+    private static final int FLING_MAX_OFF_PATH = 250;
+    private static final int FLING_THRESHOLD_VELOCITY = 200;
+
+    //Pass the fling to the gesture detector
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        super.dispatchTouchEvent(ev);
+        return gestureDetector.onTouchEvent(ev);
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,19 +67,28 @@ public class MovieViewActivity extends ActionBarActivity {
                 new GestureDetector.SimpleOnGestureListener() {
             @Override
             public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                if (Math.abs(e1.getY() - e2.getY()) > FLING_MAX_OFF_PATH) {
+                    return false;
+                }
+
                 View layout = findViewById(R.id.movie_view_layout);
                 Animation slideoutMovie;
                 int pos;
 
-                if(e1.getAxisValue(MotionEvent.AXIS_X) > e2.getAxisValue(MotionEvent.AXIS_X)) {
-                    //Left fling
-                    pos = currentPos + 1;
-                    slideoutMovie = AnimationUtils.loadAnimation(layout.getContext(),
-                            R.anim.movieview_slideout_left);
-                } else { //Right fling
-                    pos = currentPos - 1;
-                    slideoutMovie = AnimationUtils.loadAnimation(layout.getContext(),
-                            R.anim.movieview_slideout_right);
+                if (e1.getX() - e2.getX() > FLING_MIN_DISTANCE
+                    && Math.abs(velocityX) > FLING_THRESHOLD_VELOCITY) {
+                        pos = currentPos + 1;
+                        slideoutMovie = AnimationUtils.loadAnimation(layout.getContext(),
+                                R.anim.movieview_slideout_left);
+                }
+                else if (e2.getX() - e1.getX() > FLING_MIN_DISTANCE
+                    && Math.abs(velocityX) > FLING_THRESHOLD_VELOCITY) {
+                        pos = currentPos - 1;
+                        slideoutMovie = AnimationUtils.loadAnimation(layout.getContext(),
+                                R.anim.movieview_slideout_right);
+                } else {
+                    //Not a valid fling in either direction
+                    return false;
                 }
                 //Wraparound
                 if(pos < 0) {
@@ -109,23 +128,9 @@ public class MovieViewActivity extends ActionBarActivity {
         } else {
             Log.e(TAG, "Null bundle passed to movieview");
         }
-        //This should only occur if the user enters the app through the MovieView activity
-        if(!ImageLoader.getInstance().isInited()) {
-            //Create settings for and initialise nostra13's ImageLoader
-            //See: https://github.com/nostra13/Android-Universal-Image-Loader
-            DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder().
-                    cacheInMemory(true).cacheOnDisc(true).build();
-            ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
-                    .defaultDisplayImageOptions(defaultOptions).build();
-            ImageLoader.getInstance().init(config);
-        }
 
     }
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        gestureDetector.onTouchEvent(event);
-        return super.onTouchEvent(event);
-    }
+
     //Display the movie at pos on the screen.
     public void displayMovie(int pos) {
         currentPos = pos;
@@ -142,6 +147,7 @@ public class MovieViewActivity extends ActionBarActivity {
         //Hide the tagline if there isn't one, helps compact the view
         if(current.getTagline().length() > 0) {
             movieTagline.setText(current.getTagline());
+            movieTagline.setVisibility(View.VISIBLE);
         } else {
             movieTagline.setVisibility(View.GONE);
         }
@@ -177,7 +183,6 @@ public class MovieViewActivity extends ActionBarActivity {
 
     //Called when user presses 'Actors' button. Expands list of actors.
     public void onActorButtonPress(View view) {
-        //TODO: add some animation for expanding/unexpanding
         if(!actorsExpanded) {
             showActors();
         } else {
@@ -190,10 +195,13 @@ public class MovieViewActivity extends ActionBarActivity {
         actorsExpanded = true;
 
         StringBuilder display = new StringBuilder();
-        if(current.getActors().length > 0) {
-            for(String str: current.getActors()) {
-                display.append(str).append("\n");
+        String[] actorsList = current.getActors();
+        if(actorsList.length > 0) {
+            for(int i = 0; i < actorsList.length - 1; i++) {
+                display.append(actorsList[i]).append("\n");
             }
+            //Don't want a trailing \n on the last element
+            display.append(actorsList[actorsList.length - 1]);
         } else {
             display.append("No actors.");
         }
@@ -211,7 +219,6 @@ public class MovieViewActivity extends ActionBarActivity {
 
     //Called when user presses 'Directors' button. Expands list of directors.
     public void onDirectorButtonPress(View view) {
-        //TODO: add some animation for expanding/unexpanding
         if(!directorsExpanded) {
             showDirectors();
         } else {
@@ -224,10 +231,13 @@ public class MovieViewActivity extends ActionBarActivity {
         directorsExpanded = true;
 
         StringBuilder display = new StringBuilder();
+        String[] directorsList = current.getDirectors();
         if(current.getDirectors().length > 0) {
-            for(String str: current.getDirectors()) {
-                display.append(str).append("\n");
+            for(int i = 0; i < directorsList.length - 1; i++) {
+                display.append(directorsList[i]).append("\n");
             }
+            //Don't want a trailing \n on the last element
+            display.append(directorsList[directorsList.length - 1]);
         } else {
             display.append("No directors.");
         }
@@ -245,6 +255,28 @@ public class MovieViewActivity extends ActionBarActivity {
 
     public void onQualityButtonPress(View view) {
         new ChangeQualityFragment().show(getSupportFragmentManager(), null);
+    }
+
+    //Helper functions for testing (should not need to use these normally)
+    public ArrayList<Movie> getMovies() {
+        return movies;
+    }
+
+    public int getCurrentPos() {
+        return currentPos;
+    }
+
+    public void setMovies(ArrayList<Movie> moviesList, int pos) {
+        if((pos < 0) || (pos > moviesList.size())) {
+            throw new IllegalArgumentException("Position outside range of movies list");
+        }
+        movies = moviesList;
+        currentPos = pos;
+        current = moviesList.get(currentPos);
+    }
+
+    public void setCurrentPos(int pos) {
+        currentPos = pos;
     }
 
     //Popup box for user to change qualities
@@ -415,7 +447,6 @@ public class MovieViewActivity extends ActionBarActivity {
                     public void onClick(DialogInterface dialogInterface, int i) {
                     }
                 });
-
         return builder.create();
         }
         //Keep the dialog up when the user changes orientation
